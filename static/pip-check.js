fa628a28a8cd8c972a38b78d92dc02af94167b01
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-10-05 11:54:49 -0700

    ;'s everywhere

--- static/pip-check.js
+++ static/pip-check.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("#tabs").tabs();
-	$("#tabs").children("ul").append("<li class='refresh'><a class='refresh'><img class='refresh' src='/static/images/refresh.svg' height='25px' width='25px' /></a></li>")
+	$("#tabs").children("ul").append("<li class='refresh'><a class='refresh'><img class='refresh' src='/static/images/refresh.svg' height='25px' width='25px' /></a></li>");
 
 	var refresh = function() {
 		var availupdates = [];
@@ -8,8 +8,9 @@ $(document).ready(function() {
 		$(document).off('click', 'a.refresh');
 		// should also do a overlay over the update and installed pages...
 		$('a.refresh').children('img').attr('id', 'rotating');
-		$('#tabs-1').children('a.updateall').after("<img id='rotating' class='refresh' src='/static/images/refresh.svg' style='width: 250px; height: 250px; cursor: default;'>")
-		$('#tabs-2').append("<img id='rotating' class='refresh' src='/static/images/refresh.svg' style='width: 250px; height: 250px; cursor: default;'>")
+		$('#tabs-1').children('a.updateall').after("<img id='rotating' class='refresh' src='/static/images/refresh.svg' style='width: 250px; height: 250px; cursor: default;'>");
+		$('#tabs-2').append("<img id='rotating' class='refresh' src='/static/images/refresh.svg' style='width: 250px; height: 250px; cursor: default;'>");
+		$('a.updateall').css("color", "grey");
 		$.getJSON("/refresh", function(data) {
 			for (var i = 0;i<data['updates'].length;i++) {
 				if (data['updates'][i][3] === false) {
@@ -26,7 +27,7 @@ $(document).ready(function() {
 					$(".installed").append("<li class='warn' id='"+data['installed'][i][0]+"'><a class='name' href='https://pypi.python.org/pypi/"+data['installed'][i][0]+"/' target='_blank'>"+data['installed'][i][0]+"</a><br /><div class='version'>"+data['installed'][i][1]+"</div></li>");
 				}
 			}
-			// re-enable updateall if its been turned off!
+			// re-enable refresh button
 			$(document).on("click", 'a.refresh', function(event) {
 				event.preventDefault();
 				$('ul.updates').children().remove();
@@ -34,22 +35,23 @@ $(document).ready(function() {
 				avail = refresh();
 			});
 			$('a.refresh').children('img').removeAttr('id');
-			$('#tabs-1').children('img#rotating').remove()
-			$('#tabs-2').children('img#rotating').remove()
+			$('#tabs-1').children('img#rotating').remove();
+			$('#tabs-2').children('img#rotating').remove();
+			$('a.updateall').css('color', 'rgb(83, 151, 83)');
 		});
 		return availupdates;
 	}
 
 	// disable updateall button
 	var updates = refresh();
-
+    
 	// should implement a thing to check if you want to update if its a major version change!
 	$(document).on("click", 'a.update', function(event) {
 		event.preventDefault();
 		// maybe give it some updating typing logo/thing and also disable it so you cant click twice
 		$.post(event.target.href, function(data) {
 			if (data === "") {
-				// remove the div, should also update the installed list somehow, maybe change the color to indicate its been updated... (give each li a id/class with the name of the package.)
+				// remove the div, should also update the installed list somehow, maybe change the color to indicate its been updated... (give each li a id/class with the name of the package.);
 				$('li#'+event.target.href.split('/').slice(-1)[0].split("==")[0]).children('.version').text(event.target.href.split("==")[1]);
 				$('li#'+event.target.href.split('/').slice(-1)[0].split("==")[0]).css('background-color', '#85C57C');
 				$(event.target).parent().remove();
@@ -62,7 +64,7 @@ $(document).ready(function() {
 	$(document).on("click", 'a.update-warn', function(event) {
 		event.preventDefault();
 		console.log("weee");
-	})
+	});
 
 	$(document).on("click", 'a.updateall', function(event) {
 		event.preventDefault();
