15531b80ddbca0db349e24cd9a63c6f2b3500cb6
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-10-04 18:12:31 -0700

    a better refresh button

--- static/pip-check.js
+++ static/pip-check.js
@@ -1,26 +1,40 @@
 $(document).ready(function() {
 	$("#tabs").tabs();
-	$("#tabs").children("ul").append("<li class='refresh'><a class='refresh' href='/refresh'><img class='refresh' src='/static/images/refresh.svg' height='25px' width='25px' /></a></li>")
+	$("#tabs").children("ul").append("<li class='refresh'><a class='refresh'><img class='refresh' src='/static/images/refresh.svg' height='25px' width='25px' /></a></li>")
 
-	var availupdates = []
+	var refresh = function() {
+		var availupdates = [];
+		// disable update all button
+		$(document).off('click', 'a.refresh')
+		$.getJSON("/refresh", function(data) {
+			for (var i = 0;i<data['updates'].length;i++) {
+				if (data['updates'][i][3] === false) {
+					$(".updates").append("<li><a class='name' href='https://pypi.python.org/pypi/"+data['updates'][i][0]+"/' target='_blank'>"+data['updates'][i][0]+"</a><br /><div class='old'>"+data['updates'][i][1]+"</div>   →   <div class='new'>"+data['updates'][i][2]+"</div><br /><a class='update' href='/update/"+data['updates'][i][0]+"=="+data['updates'][i][2]+"'>update</a></li>");
+				} else {
+					$(".updates").append("<li class='warn'><a class='name' href='https://pypi.python.org/pypi/"+data['updates'][i][0]+"/' target='_blank'>"+data['updates'][i][0]+"</a><br /><div class='old'>"+data['updates'][i][1]+"</div>   →   <div class='new'>"+data['updates'][i][2]+"</div><br /><a class='warn-update' href='/update/"+data['updates'][i][0]+"=="+data['updates'][i][2]+"'>update</a></li>");
+				}
+				availupdates.push(data['updates'][i][0])
+			}
+			for (var i = 0;i<data['installed'].length;i++) {
+				if (data['installed'][i][2] === false) {
+					$(".installed").append("<li id='"+data['installed'][i][0]+"'><a class='name' href='https://pypi.python.org/pypi/"+data['installed'][i][0]+"/' target='_blank'>"+data['installed'][i][0]+"</a><br /><div class='version'>"+data['installed'][i][1]+"</div></li>");
+				} else {
+					$(".installed").append("<li class='warn' id='"+data['installed'][i][0]+"'><a class='name' href='https://pypi.python.org/pypi/"+data['installed'][i][0]+"/' target='_blank'>"+data['installed'][i][0]+"</a><br /><div class='version'>"+data['installed'][i][1]+"</div></li>");
+				}
+			}
+			// re-enable updateall if its been turned off!
+			$(document).on("click", 'a.refresh', function(event) {
+				event.preventDefault();
+				$('ul.updates').children().remove();
+				$('ul.installed').children().remove();
+				avail = refresh();
+			})
+		});
+		return availupdates;
+	}
 
-	$.getJSON("/refresh", function(data) {
-		for (var i = 0;i<data['updates'].length;i++) {
-			if (data['updates'][i][3] === false) {
-				$(".updates").append("<li><a class='name' href='https://pypi.python.org/pypi/"+data['updates'][i][0]+"/' target='_blank'>"+data['updates'][i][0]+"</a><br /><div class='old'>"+data['updates'][i][1]+"</div>   →   <div class='new'>"+data['updates'][i][2]+"</div><br /><a class='update' href='/update/"+data['updates'][i][0]+"=="+data['updates'][i][2]+"'>update</a></li>");
-			} else {
-				$(".updates").append("<li class='warn'><a class='name' href='https://pypi.python.org/pypi/"+data['updates'][i][0]+"/' target='_blank'>"+data['updates'][i][0]+"</a><br /><div class='old'>"+data['updates'][i][1]+"</div>   →   <div class='new'>"+data['updates'][i][2]+"</div><br /><a class='warn-update' href='/update/"+data['updates'][i][0]+"=="+data['updates'][i][2]+"'>update</a></li>");
-			}
-			availupdates.push(data['updates'][i][0])
-		}
-		for (var i = 0;i<data['installed'].length;i++) {
-			if (data['installed'][i][2] === false) {
-				$(".installed").append("<li id='"+data['installed'][i][0]+"'><a class='name' href='https://pypi.python.org/pypi/"+data['installed'][i][0]+"/' target='_blank'>"+data['installed'][i][0]+"</a><br /><div class='version'>"+data['installed'][i][1]+"</div></li>");
-			} else {
-				$(".installed").append("<li class='warn' id='"+data['installed'][i][0]+"'><a class='name' href='https://pypi.python.org/pypi/"+data['installed'][i][0]+"/' target='_blank'>"+data['installed'][i][0]+"</a><br /><div class='version'>"+data['installed'][i][1]+"</div></li>");
-			}
-		}
-	});
+	// disable updateall button
+	var updates = refresh();
 
 	// should implement a thing to check if you want to update if its a major version change!
 	$(document).on("click", 'a.update', function(event) {
@@ -56,10 +70,5 @@ $(document).ready(function() {
 				// ERRORRRR
 			}
 		});
-	})
-
-	$(document).on("click", 'a.refresh', function(event) {
-		event.preventDefault();
-		console.log("weee");
 	})
 });
