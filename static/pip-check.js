b8dc372523b71ea4bcd955acc99762264768df84
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-10-05 12:40:59 -0700

    different style for displaying stuff...

--- static/pip-check.js
+++ static/pip-check.js
@@ -14,17 +14,17 @@ $(document).ready(function() {
 		$.getJSON("/refresh", function(data) {
 			for (var i = 0;i<data['updates'].length;i++) {
 				if (data['updates'][i][3] === false) {
-					$(".updates").append("<li><a class='name' href='https://pypi.python.org/pypi/"+data['updates'][i][0]+"/' target='_blank'>"+data['updates'][i][0]+"</a><br /><div class='old'>"+data['updates'][i][1]+"</div>   →   <div class='new'>"+data['updates'][i][2]+"</div><br /><a class='update' id='"+data['updates'][i][0]+"' href='/update/"+data['updates'][i][0]+"=="+data['updates'][i][2]+"'>update</a></li>");
+					$(".updates").append("<li><div class='list-wrap'><a class='name' href='https://pypi.python.org/pypi/"+data['updates'][i][0]+"/' target='_blank'>"+data['updates'][i][0]+"</a></div><div class='list-wrap'><div class='old'>"+data['updates'][i][1]+"</div>   →   <div class='new'>"+data['updates'][i][2]+"</div></div><div class='list-wrap'><a class='update' id='"+data['updates'][i][0]+"' href='/update/"+data['updates'][i][0]+"=="+data['updates'][i][2]+"'>update</a></div></li>");
 				} else {
-					$(".updates").append("<li class='warn'><a class='name' href='https://pypi.python.org/pypi/"+data['updates'][i][0]+"/' target='_blank'>"+data['updates'][i][0]+"</a><br /><div class='old'>"+data['updates'][i][1]+"</div>   →   <div class='new'>"+data['updates'][i][2]+"</div><br /><a class='warn-update' id='"+data['updates'][i][0]+"' href='/update/"+data['updates'][i][0]+"=="+data['updates'][i][2]+"'>update</a></li>");
+					$(".updates").append("<li class='warn'><div class='list-wrap'><a class='name' href='https://pypi.python.org/pypi/"+data['updates'][i][0]+"/' target='_blank'>"+data['updates'][i][0]+"</a></div><div class='list-wrap'>><div class='old'>"+data['updates'][i][1]+"</div>   →   <div class='new'>"+data['updates'][i][2]+"</div></div><div class='list-wrap'><a class='warn-update' id='"+data['updates'][i][0]+"' href='/update/"+data['updates'][i][0]+"=="+data['updates'][i][2]+"'>update</a></div></li>");
 				}
 				availupdates.push(data['updates'][i][0])
 			}
 			for (var i = 0;i<data['installed'].length;i++) {
 				if (data['installed'][i][2] === false) {
-					$(".installed").append("<li id='"+data['installed'][i][0]+"'><a class='name' href='https://pypi.python.org/pypi/"+data['installed'][i][0]+"/' target='_blank'>"+data['installed'][i][0]+"</a><br /><div class='version'>"+data['installed'][i][1]+"</div></li>");
+					$(".installed").append("<li id='"+data['installed'][i][0]+"'><div class='list-wrap2'><a class='name' href='https://pypi.python.org/pypi/"+data['installed'][i][0]+"/' target='_blank'>"+data['installed'][i][0]+"</a></div><div class='list-wrap2'><div class='version'>"+data['installed'][i][1]+"</div></div></li>");
 				} else {
-					$(".installed").append("<li class='warn' id='"+data['installed'][i][0]+"'><a class='name' href='https://pypi.python.org/pypi/"+data['installed'][i][0]+"/' target='_blank'>"+data['installed'][i][0]+"</a><br /><div class='version'>"+data['installed'][i][1]+"</div></li>");
+					$(".installed").append("<li class='warn' id='"+data['installed'][i][0]+"'><div class='list-wrap2'><a class='name' href='https://pypi.python.org/pypi/"+data['installed'][i][0]+"/' target='_blank'>"+data['installed'][i][0]+"</a></div><div class='list-wrap'><div class='version'>"+data['installed'][i][1]+"</div></div></li>");
 				}
 			}
 			// re-enable refresh button
@@ -42,7 +42,7 @@ $(document).ready(function() {
 		return availupdates;
 	}
 
-	// disable updateall button
+	// initial info
 	var updates = refresh();
     
 	// should implement a thing to check if you want to update if its a major version change!
@@ -54,7 +54,7 @@ $(document).ready(function() {
 				// remove the div, should also update the installed list somehow, maybe change the color to indicate its been updated... (give each li a id/class with the name of the package.);
 				$('li#'+event.target.href.split('/').slice(-1)[0].split("==")[0]).children('.version').text(event.target.href.split("==")[1]);
 				$('li#'+event.target.href.split('/').slice(-1)[0].split("==")[0]).css('background-color', '#85C57C');
-				$(event.target).parent().remove();
+				$(event.target).parent().parent().remove();
 			} else {
 				// ERRORRRR
 			}
